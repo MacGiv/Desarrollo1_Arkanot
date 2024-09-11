be7b441f78c6ec4ec9295f65faeb264a83d2f433
MacGiv <tomasluchelli.17@gmail.com>
2024-09-11 13:08:14 -0300

    Class tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,25 @@
 
 int main(int args, char* argv[])
 {
-    // set up our window and a few resources we need
-    slWindow(1280, 720, "Simple SIGIL Example", false);
+    int screenWidth = 1280;
+    int screenHeight = 720;
     
+    int paddleHeight = 50;
+    int paddleWidth = 150;
+
+    // set up our window and a few resources we need
+    slWindow(screenWidth, screenHeight, "Simple SIGIL Example", false);
+    
+
+
 
     while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
     {
         // background glow
-        slSetForeColor(0.1, 0.9, 0.2, 0.4);
+        slSetForeColor(1.0, 1.0, 1.0, 1.0);
         
+
+        slRectangleFill(screenWidth / 2, screenHeight / 2, paddleWidth, paddleHeight);
         
 
         slRender();
